daf8b710287ef5cb9b96fcf22afbbb1fccc5f703
Taine Collins <tainecollins@Taines-MacBook-Pro.local>
2019-08-12 19:16:23 +1200

    article-app-pt2

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
 import React, {Component} from 'react';
 import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
+import Tab from 'react-bootstrap/Tab';
 import './App.css';
+
+var APIkey = '498cb3731a91441784951e1fd976bdc6';
+
+var key = '?apiKey='+APIkey;
 
 class App extends Component{
   constructor(props){
     super(props)
+    this.state = {
+      activeKey: 'business',
+      articles: [
+
+      ],
+    }
+  }
+
+  handleTabSelect = (key, e) => {
+    this.setState({activeKey: key});
+  }
+
+  handleSearchClick = (e) => {
+    e.preventDefault();
+    this.setState({activeKey: 'search'});
   }
 
   render(){
     return(
       <div className="container">
+        <Tab.Container activeKey={this.state.activeKey} onSelect={this.handleTabSelect}>
         <div className="searchbar">
           <input type="text" placeholder="search keywords" className="search"/>
-          <Button variant="primary" className="btn">Search</Button>
+          <Button variant="primary" className="btn" onClick={this.handleSearchClick} type="submit">Search</Button>
         </div>
         <div className="articles-layout">
           <Nav className="nav-items flex-column">
@@ -27,21 +48,52 @@ class App extends Component{
               <Nav.Link eventKey="technology" className="item">Technology</Nav.Link>
             </Nav.Item>
           </Nav>
-          <div className="articles">
-            <div className="article">
-              <h2>Lorem, ipsum.</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sit ex reprehenderit voluptatem quod corrupti nobis impedit perferendis repellat id.</p>
+          <Tab.Content>
+            <Tab.Pane eventKey="business">
+            <div className="articles">
+              <h1>Business</h1>
+              <div className="article">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sit ex reprehenderit voluptatem quod corrupti nobis impedit perferendis repellat id.</p>
+              </div>
+              <div className="article">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sit ex reprehenderit voluptatem quod corrupti nobis impedit perferendis repellat id.</p>
+              </div>
+              <div className="article">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sit ex reprehenderit voluptatem quod corrupti nobis impedit perferendis repellat id.</p>
+              </div>
             </div>
-            <div className="article">
-              <h2>Lorem, ipsum.</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sit ex reprehenderit voluptatem quod corrupti nobis impedit perferendis repellat id.</p>
+            </Tab.Pane>
+            <Tab.Pane eventKey="science">
+            <div className="articles">
+              <h1>Science</h1>
+              <div className="article">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sit ex reprehenderit voluptatem quod corrupti nobis impedit perferendis repellat id.</p>
+              </div>
+              <div className="article">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sit ex reprehenderit voluptatem quod corrupti nobis impedit perferendis repellat id.</p>
+              </div>
+              <div className="article">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sit ex reprehenderit voluptatem quod corrupti nobis impedit perferendis repellat id.</p>
+              </div>
             </div>
-            <div className="article">
-              <h2>Lorem, ipsum.</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sit ex reprehenderit voluptatem quod corrupti nobis impedit perferendis repellat id.</p>
+            </Tab.Pane>
+            <Tab.Pane eventKey="technology">
+            <div className="articles">
+              <h1>Technology</h1>
+              <div className="article">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sit ex reprehenderit voluptatem quod corrupti nobis impedit perferendis repellat id.</p>
+              </div>
+              <div className="article">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sit ex reprehenderit voluptatem quod corrupti nobis impedit perferendis repellat id.</p>
+              </div>
+              <div className="article">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur sit ex reprehenderit voluptatem quod corrupti nobis impedit perferendis repellat id.</p>
+              </div>
             </div>
-          </div>
+            </Tab.Pane>
+          </Tab.Content>
         </div>
+        </Tab.Container>
       </div>
     )
   }
